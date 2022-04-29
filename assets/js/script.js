ba5badc7c1664b58d5f28495c0ea5361fb9e5c3f
unknown <joyprince942003@gmail.com>
2022-04-29 11:01:08 +0530

    Vedhanthi-Final

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,468 @@
 //       touch: true,
 
 //   });
- 
+
+/* -------------------------------------
+----------------------------------------
+MOBILE APP
+----------------------------------------
+--------------------------------------*/
+
+$('.mad-self').click(function(){ 
+  $('.nativeapp1').fadeOut('fast');
+  $('.mad').fadeIn(1000);
+});
+
+$('.mad-self').click(function(){ 
+  $('.hybridapp1').fadeOut('fast');
+  $('.mad').fadeIn(1000);
+});
+
+$('.mad-self').click(function(){ 
+  $('.kotilin1').fadeOut('fast');
+  $('.mad').fadeIn(1000);
+});
+
+$('.native-self').click(function(){ 
+  $('.mad').fadeOut('fast');
+  $('.nativeapp1').fadeIn(1000);
+});
+
+$('.native-self').click(function(){ 
+  $('.hybridapp1').fadeOut('fast');
+  $('.nativeapp1').fadeIn(1000);
+});
+
+$('.native-self').click(function(){ 
+  $('.kotilin1').fadeOut('fast');
+  $('.nativeapp1').fadeIn(1000);
+});
+
+
+
+$('.hybrid-self').click(function(){ 
+  $('.mad').fadeOut('fast');
+  $('.hybridapp1').fadeIn(1000);
+});
+
+
+$('.hybrid-self').click(function(){ 
+  $('.nativeapp1').fadeOut('fast');
+  $('.hybridapp1').fadeIn(1000);
+});
+
+$('.hybrid-self').click(function(){ 
+  $('.kotilin1').fadeOut('fast');
+  $('.hybridapp1').fadeIn(1000);
+});
+
+
+
+$('.kotilin-self').click(function(){ 
+  $('.mad').fadeOut('fast');
+  $('.kotilin1').fadeIn(1000);
+});
+
+$('.kotilin-self').click(function(){ 
+  $('.hybridapp1').fadeOut('fast');
+  $('.kotilin 1').fadeIn(1000);
+});
+
+$('.kotilin-self').click(function(){ 
+  $('.nativeapp1').fadeOut('fast');
+  $('.kotilin 1').fadeIn(1000);
+});
+
+
+/* -------------------------------------
+----------------------------------------
+TEST SERVICE
+----------------------------------------
+--------------------------------------*/
+
+$('.test-self').click(function(){ 
+  $('.software').fadeOut('fast');
+  $('.test').fadeIn(1000);
+});
+
+$('.test-self').click(function(){ 
+  $('.automation').fadeOut('fast');
+  $('.test').fadeIn(1000);
+});
+
+$('.test-self').click(function(){ 
+  $('.security').fadeOut('fast');
+  $('.test').fadeIn(1000);
+});
+
+$('.test-self').click(function(){ 
+  $('.performance').fadeOut('fast');
+  $('.test').fadeIn(1000);
+});
+
+$('.test-self').click(function(){ 
+  $('.embedded').fadeOut('fast');
+  $('.test').fadeIn(1000);
+});
+
+$('.test-self').click(function(){ 
+  $('.mobiletest').fadeOut('fast');
+  $('.test').fadeIn(1000);
+});
+
+
+$('.software-self').click(function(){ 
+  $('.test').fadeOut('fast');
+  $('.software').fadeIn(1000);
+});
+
+
+$('.software-self').click(function(){ 
+  $('.automation').fadeOut('fast');
+  $('.software').fadeIn(1000);
+});
+
+$('.software-self').click(function(){ 
+  $('.performance').fadeOut('fast');
+  $('.software').fadeIn(1000);
+});
+
+$('.software-self').click(function(){ 
+  $('.embedded').fadeOut('fast');
+  $('.software').fadeIn(1000);
+});
+
+$('.software-self').click(function(){ 
+  $('.security').fadeOut('fast');
+  $('.software').fadeIn(1000);
+});
+
+
+$('.software-self').click(function(){ 
+  $('.mobiletest').fadeOut('fast');
+  $('.software').fadeIn(1000);
+});
+
+
+$('.automation-self').click(function(){ 
+  $('.test').fadeOut('fast');
+  $('.automation').fadeIn(1000);
+});
+
+$('.automation-self').click(function(){ 
+  $('.software').fadeOut('fast');
+  $('.automation').fadeIn(1000);
+});
+
+$('.automation-self').click(function(){ 
+  $('.security').fadeOut('fast');
+  $('.automation').fadeIn(1000);
+});
+
+$('.automation-self').click(function(){ 
+  $('.performance').fadeOut('fast');
+  $('.automation').fadeIn(1000);
+});
+
+$('.automation-self').click(function(){ 
+  $('.embedded').fadeOut('fast');
+  $('.automation').fadeIn(1000);
+});
+
+$('.automation-self').click(function(){ 
+  $('.mobiletest').fadeOut('fast');
+  $('.automation').fadeIn(1000);
+});
+
+
+$('.security-self').click(function(){ 
+  $('.test').fadeOut('fast');
+  $('.security').fadeIn(1000);
+});
+
+$('.security-self').click(function(){ 
+  $('.software').fadeOut('fast');
+  $('.security').fadeIn(1000);
+});
+
+$('.security-self').click(function(){ 
+  $('.automation').fadeOut('fast');
+  $('.security').fadeIn(1000);
+});
+
+$('.security-self').click(function(){ 
+  $('.performance').fadeOut('fast');
+  $('.security').fadeIn(1000);
+});
+
+$('.security-self').click(function(){ 
+  $('.embedded').fadeOut('fast');
+  $('.security').fadeIn(1000);
+});
+
+$('.security-self').click(function(){ 
+  $('.mobiletest').fadeOut('fast');
+  $('.security').fadeIn(1000);
+});
+
+$('.performance-self').click(function(){ 
+  $('.test').fadeOut('fast');
+  $('.performance').fadeIn(1000);
+});
+
+$('.performance-self').click(function(){ 
+  $('.test, .software, .embedded, .security, .mobiletest, .automation').fadeOut('fast');
+  $('.performance').fadeIn(1000);
+});
+
+$('.embedded-self').click(function(){ 
+  $('.test, .software, .performance, .security, .mobiletest, .automation').fadeOut('fast');
+  $('.embedded').fadeIn(1000);
+});
+
+$('.mobile-self').click(function(){ 
+  $('.test, .software, .performance, .security, .embedded, .automation').fadeOut('fast');
+  $('.mobiletest').fadeIn(1000);
+});
+
+/*--------------------------------
+
+design
+
+------------------------*/
+
+$('.design-self').click(function(){ 
+  $('.web, .ui, .logo').fadeOut('fast');
+  $('.design').fadeIn(1000);
+});
+
+$('.web-self').click(function(){ 
+  $('.design, .ui, .logo').fadeOut('fast');
+  $('.web').fadeIn(1000);
+});
+
+$('.ui-self').click(function(){ 
+  $('.web, .design, .logo').fadeOut('fast');
+  $('.ui').fadeIn(1000);
+});
+
+$('.logo-self').click(function(){ 
+  $('.web, .ui, .design').fadeOut('fast');
+  $('.logo').fadeIn(1000);
+});
+
+/*--------------------------------
+
+Open Source
+
+------------------------*/
+
+$('.os-self').click(function(){ 
+  $('.php, .cake, .mysql, .angular, .vue, .node, .wp, .laravel, .react').fadeOut('fast');
+  $('.os').fadeIn(1000);
+});
+
+$('.php-self').click(function(){ 
+  $('.os, .cake, .mysql, .angular, .vue, .node, .wp, .laravel, .react').fadeOut('fast');
+  $('.php').fadeIn(1000);
+});
+
+$('.cake-self').click(function(){ 
+  $('.os, .php, .mysql, .angular, .vue, .node, .wp, .laravel, .react').fadeOut('fast');
+  $('.cake').fadeIn(1000);
+});
+
+$('.mysql-self').click(function(){ 
+  $('.os, .cake, .php, .angular, .vue, .node, .wp, .laravel, .react').fadeOut('fast');
+  $('.mysql').fadeIn(1000);
+});
+
+$('.angular-self').click(function(){ 
+  $('.os, .cake, .mysql, .php, .vue, .node, .wp, .laravel, .react').fadeOut('fast');
+  $('.angular').fadeIn(1000);
+});
+
+$('.vue-self').click(function(){ 
+  $('.os, .cake, .mysql, .angular, .php, .node, .wp, .laravel, .react').fadeOut('fast');
+  $('.vue').fadeIn(1000);
+});
+
+$('.node-self').click(function(){ 
+  $('.os, .cake, .mysql, .angular, .vue, .php, .wp, .laravel, .react').fadeOut('fast');
+  $('.node').fadeIn(1000);
+});
+
+$('.wp-self').click(function(){ 
+  $('.os, .cake, .mysql, .angular, .vue, .node, .php, .laravel, .react').fadeOut('fast');
+  $('.wp').fadeIn(1000);
+});
+
+$('.laravel-self').click(function(){ 
+  $('.os, .cake, .mysql, .angular, .vue, .node, .wp, .php, .react').fadeOut('fast');
+  $('.laravel').fadeIn(1000);
+});
+
+$('.react-self').click(function(){ 
+  $('.os, .cake, .mysql, .angular, .vue, .node, .wp, .laravel, .php').fadeOut('fast');
+  $('.react').fadeIn(1000);
+});
+
+/*--------------------------------
+
+microsoft
+
+------------------------*/
+
+$('.microsoft-self').click(function(){ 
+  $('.asp, .sql, .silver, .devops, .c').fadeOut('fast');
+  $('.microsoft').fadeIn(1000);
+});
+
+$('.asp-self').click(function(){ 
+  $('.microsoft, .sql, .silver, .devops, .c').fadeOut('fast');
+  $('.asp').fadeIn(1000);
+});
+
+$('.sql-self').click(function(){ 
+  $('.microsoft, .asp, .silver, .devops, .c').fadeOut('fast');
+  $('.sql').fadeIn(1000);
+});
+
+$('.silver-self').click(function(){ 
+  $('.microsoft, .sql, .asp, .devops, .c').fadeOut('fast');
+  $('.silver').fadeIn(1000);
+});
+
+$('.devops-self').click(function(){ 
+  $('.microsoft, .sql, .silver, .asp, .c').fadeOut('fast');
+  $('.devops').fadeIn(1000);
+});
+
+$('.c-self').click(function(){ 
+  $('.microsoft, .sql, .silver, .devops, .asp').fadeOut('fast');
+  $('.c').fadeIn(1000);
+});
+
+/* -------------------------------------
+----------------------------------------
+NATIVE APP
+----------------------------------------
+--------------------------------------*/
+
+$('.native1-self').click(function(){ 
+  $('.android, .ios').fadeOut('fast');
+  $('.native').fadeIn(1000);
+});
+
+$('.android-self').click(function(){ 
+  $('.native, .ios').fadeOut('fast');
+  $('.android').fadeIn(1000);
+});
+
+$('.ios-self').click(function(){ 
+  $('.android, .native').fadeOut('fast');
+  $('.ios').fadeIn(1000);
+});
+
+/* -------------------------------------
+----------------------------------------
+HYBRID APP
+----------------------------------------
+--------------------------------------*/
+
+$('.hybrid1-self').click(function(){ 
+  $('.react-hybrid, .xamarian, .phonegap, .flutter, .ionic').fadeOut('fast');
+  $('.hybrid').fadeIn(1000);
+});
+
+$('.react-self').click(function(){ 
+  $('.hybrid, .xamarian, .phonegap, .flutter, .ionic').fadeOut('fast');
+  $('.react-hybrid').fadeIn(1000);
+});
+
+$('.xamarian-self').click(function(){ 
+  $('.hybrid, .react-hybrid, .phonegap, .flutter, .ionic').fadeOut('fast');
+  $('.xamarian').fadeIn(1000);
+});
+
+$('.phonegap-self').click(function(){ 
+  $('.hybrid, .react-hybrid, .xamarian, .flutter, .ionic').fadeOut('fast');
+  $('.phonegap').fadeIn(1000);
+});
+
+$('.flutter-self').click(function(){ 
+  $('.react-hybrid, .xamarian, .phonegap, .hybrid, .ionic').fadeOut('fast');
+  $('.flutter').fadeIn(1000);
+});
+
+$('.ionic-self').click(function(){ 
+  $('.react-hybrid, .xamarian, .phonegap, .flutter, .hybrid').fadeOut('fast');
+  $('.ionic').fadeIn(1000);
+});
+
+/* -------------------------------------
+----------------------------------------
+qa
+----------------------------------------
+--------------------------------------*/
+
+$('.qa-self').click(function(){ 
+  $('.cypress, .katalon, .selenium, .cucumber, .test-project, .jmeter, .jenkins ').fadeOut('fast');
+  $('.qa').fadeIn(1000);
+});
+
+$('.cypress-self').click(function(){ 
+  $('.qa, .katalon, .selenium, .cucumber, .test-project, .jmeter, .jenkins ').fadeOut('fast');
+  $('.cypress').fadeIn(1000);
+});
+
+$('.katalon-self').click(function(){ 
+  $('.cypress, .qa, .selenium, .cucumber, .test-project, .jmeter, .jenkins ').fadeOut('fast');
+  $('.katalon').fadeIn(1000);
+});
+
+$('.selenium-self').click(function(){ 
+  $('.cypress, .katalon, .qa, .cucumber, .test-project, .jmeter, .jenkins ').fadeOut('fast');
+  $('.selenium').fadeIn(1000);
+});
+
+$('.cucumber-self').click(function(){ 
+  $('.cypress, .katalon, .selenium, .qa, .test-project, .jmeter, .jenkins ').fadeOut('fast');
+  $('.cucumber').fadeIn(1000);
+});
+
+$('.test-project-self').click(function(){ 
+  $('.cypress, .katalon, .selenium, .cucumber, .qa, .jmeter, .jenkins ').fadeOut('fast');
+  $('.test-project').fadeIn(1000);
+});
+
+$('.jmeter-self').click(function(){ 
+  $('.cypress, .katalon, .selenium, .cucumber, .test-project, .qa, .jenkins ').fadeOut('fast');
+  $('.jmeter').fadeIn(1000);
+});
+
+$('.jenkins-self').click(function(){ 
+  $('.cypress, .katalon, .selenium, .cucumber, .test-project, .jmeter, .qa ').fadeOut('fast');
+  $('.jenkins').fadeIn(1000);
+});
+
+
+var mybutton = document.getElementById('myBtn');
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+      mybutton.style.display = "block";
+    } else {
+      mybutton.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+
+
     AOS.init({
       offset: 70,
       delay: 200, 
@@ -31,3 +492,9 @@
         fileChosen.textContent = this.files[0].name
     })
 
+
+    $('.carousel').carousel({
+      interval: 2000
+    })
+
+    
