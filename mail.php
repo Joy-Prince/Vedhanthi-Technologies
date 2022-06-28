a7cb49ce53d9ab8230686fa29b0156da53716415
prince-joy-prince <joyprince942003@gmail.com>
2022-06-28 10:50:54 +0530

    new

--- mail.php
+++ mail.php
@@ -3,14 +3,16 @@
 
 $name = $_POST['name'];
 $email= $_POST['email'];
-$phone= $_POST['phone'];
+$phone= $_POST['phone-number'];
 $message= $_POST['message'];
 $to = "joyprince942003@gmail.com";
 $subject = "Mail From VedhanthiTechnologies.com";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Phone Number = " . $phone . "\r\n Message =" . $message;
 $headers = "From: form@vedhanthitechnologies.com" . "\r\n" .
 "CC: somebodyelse@example.com";
-
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
 //redirect
 header("Location:thankyou.html");
 ?>
